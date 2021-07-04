3e20b49e441383dca66a4b826030b836017a158a
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-03 21:18:09 -0700

    planetCal();test pass

--- src/js/galactic.js
+++ src/js/galactic.js
@@ -3,7 +3,27 @@ export default class Person {
     this.age = age;
     this.lifeExp = lifeExp;
   }
-
+  planetCal(){
+    let planetAge = [];
+    let planetExp = [];
+    let age = this.age;
+    let lifeExp = this.lifeExp;
+    let merYears = Math.ceil(365 * .24);
+    let venYears = Math.ceil(365 * .62);
+    let marsYears = Math.ceil(365 * 1.88);
+    let jupYears = Math.ceil(365 * 11.86);
+    let planetYearArray = [merYears,venYears,marsYears,jupYears];
+    planetYearArray.forEach(function(element){
+      planetAge.push(element * age);
+    });
+    planetYearArray.forEach(function(element){
+      planetExp.push(element * lifeExp);
+    });
+    return {
+      planetAge,
+      planetExp,
+    };
+  }
   personRemYears() {
     let remYearsEarth = this.lifeExp - this.age;
     let remYearsMercury = lifeExp[1] - personAge[1];
