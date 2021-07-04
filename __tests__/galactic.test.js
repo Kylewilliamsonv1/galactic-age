9c07280d36f638ea37366b7c5428f87a1209c6bc
Kylewilliamsonv1 <84644678+Kylewilliamsonv1@users.noreply.github.com>
2021-07-03 22:29:34 -0700

    planetCal(); revised test fail

--- __tests__/galactic.test.js
+++ __tests__/galactic.test.js
@@ -11,18 +11,13 @@ describe('Person', () =>{
     expect(person.age).toEqual(10);
     expect(person.lifeExp).toEqual(100);
   });
-  test('it will return an array of remaining years to live from different planets', ()=>{
-    expect(person.personRemYears()).toEqual([90,373,144,48,8]);
+  test('it will return an array of year equivelants by planet', ()=>{
+    expect(person.planetCal()).toEqua();
   });
   test('it will tell the user if they have surpased their expected years to live and return how many years past the have lived past.', ()=>{
     expect(person.lifeCondition()).toEqual();
   });
-  test('it will return a users age, life expectancy, and remaining years in an array.', ()=>{
-    expect(person.checkStats()).toEqual([[10, 100, 90], [42, 415, 373], [17, 161, 144], [6, 54, 48], [1, 9, 8]]);
-  });
-  test('it will return an array of ages and life expectancies by planet.', ()=>{
-    expect(person.planetCal()).toEqual({"planetAge": [880, 2270, 6870, 43290], "planetExp": [8800, 22700, 68700, 432900]});
-  });
+
 });
 
 
--- src/js/galactic.js
+++ src/js/galactic.js
@@ -3,53 +3,14 @@ export default class Person {
     this.age = age;
     this.lifeExp = lifeExp;
   }
-  planetCal(){
-    let planetAge = [];
-    let planetExp = [];
-    let age = this.age;
-    let lifeExp = this.lifeExp;
-    let merYears = Math.ceil(365 * .24);
-    let venYears = Math.ceil(365 * .62);
-    let marsYears = Math.ceil(365 * 1.88);
-    let jupYears = Math.ceil(365 * 11.86);
-    let planetYearArray = [merYears,venYears,marsYears,jupYears];
-    planetYearArray.forEach(function(element){
-      planetAge.push(element * age);
-    });
-    planetYearArray.forEach(function(element){
-      planetExp.push(element * lifeExp);
-    });
-    return {
-      planetAge,
-      planetExp,
-    };
-  }
-  personRemYears() {
-    let remYearsEarth = this.lifeExp - this.age;
-    let remYearsMercury = lifeExp[1] - personAge[1];
-    let remYearsVenus = lifeExp[2] - personAge[2];
-    let remYearsMars = lifeExp[3] - personAge[3];
-    let remYearsJupiter = lifeExp[4] - personAge[4];
-    let planetRemYArray = [remYearsEarth, remYearsMercury, remYearsVenus, remYearsMars, remYearsJupiter];
-    return planetRemYArray;
-  }
+
+
+
   lifeCondition() {
     let personLCondtion = this.lifeExp - this.age;
     if (this.lifeExp < this.age) {
       return personLCondtion;
     }
-  }
-  checkStats() {
-    let personAge = this.ageCal();
-    let lifeExp = this.lifeExpCal();
-    let personRY = this.personRemYears();
-    let earthStats = [personAge[0],lifeExp[0],personRY[0]];
-    let mercuryStats = [personAge[1],lifeExp[1],personRY[1]];
-    let venusStats = [personAge[2],lifeExp[2],personRY[2]];
-    let marsStats = [personAge[3],lifeExp[3],personRY[3]];
-    let jupiterStats = [personAge[4],lifeExp[4],personRY[4]];
-    let personGalacticStats = [earthStats,mercuryStats,venusStats,marsStats,jupiterStats];
-    return personGalacticStats;
   }
 }
 
